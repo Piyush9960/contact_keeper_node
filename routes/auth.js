e93685dc18cb2b465326697e47bb3e30f6e8bcb8
Piyush Vinchurkar <piyushvinchurkar10@gmail.com>
2020-06-05 09:13:37 +0530

    express server set up with initial routes

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+// @route     GET api/auth
+// @desc      get logged in user
+// @access    private
+router.get("/", (req, res) => {
+  res.send("get logged in User");
+});
+
+// @route     POST api/auth
+// @desc      get logged in user
+// @access    private
+router.post("/", (req, res) => {
+  res.send("Log in user");
+});
+
+module.exports = router;
